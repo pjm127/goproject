23513fe34bb768bf519a135054dca723f19395c8
pjm127 <mikeywc40@gmail.com>
2024-05-02 21:53:47 +0900

    first commit

---
+++ ex4.1/ex4.1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int = 10
+	var msg string = "hello varialbe"
+	a= 20
+	msg = "good moring"
+	fmt.Println(msg,a)
+}
---
+++ ex4.3/ex4.3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	a:= 3
+	var b int
+	c:=4
+	d:=5
+	fmt.Println(a,b,c,d)
+}
